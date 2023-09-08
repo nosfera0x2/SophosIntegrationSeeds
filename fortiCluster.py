93a05cc086845e037e2d6eec02393f995214f97d
nosfera0x2 <82817752+nosfera0x2@users.noreply.github.com>
2023-09-08 13:11:36 -0500

    Update fortiCluster.py

--- fortiCluster.py
+++ fortiCluster.py
@@ -1,58 +1,56 @@
+import socket
+import time
 import re
-import datetime
 import requests
 
-# This only works on Fortinet Logs.
+# Define the syslog server address and port
+syslog_server_address = ('syslog-ip', syslog-port)
 
-# Define a regular expression pattern to match the timestamp
-pattern = r'[A-Za-z]{3} \d{2} \d{2}:\d{2}:\d{2}'
-
-# GitHub URL of the syslog file
-github_url = 'https://raw.githubusercontent.com/nosfera0x2/fileZone/main/Fortinet-Grouped-Sept1.txt'
-
-# Fetch the syslog data from the GitHub URL
-response = requests.get(github_url)
-if response.status_code == 200:
-    syslog_data = response.text.split('\n')
-else:
-    print(f"Failed to fetch data from {github_url}")
-    syslog_data = []
-
-# Process the syslog data
-for i, line in enumerate(syslog_data):
-    # Find the timestamp within the syslog message
-    match = re.search(pattern, line)
-
+# Function to update the timestamp and FTNTFGTeventtime in the syslog line
+def update_syslog_line(syslog_line):
+    current_time = time.strftime("%b %d %H:%M:%S")
+    current_time_ns = int(time.time() * 1e9)
+    
+    # Find the number inside the angle brackets
+    match = re.search(r'(<\d+>)', syslog_line)
     if match:
-        # Get the matched timestamp
-        timestamp_str = match.group()
+        prefix = match.group(1)
+    else:
+        prefix = "<185>"  # Default prefix if not found
+    
+    # Remove the original date and time, then add the new timestamp
+    syslog_line = re.sub(r'^<[0-9]+>[A-Za-z]{3} \d{2} \d{2}:\d{2}:\d{2}', f'{prefix}{current_time}', syslog_line)
+    
+    # Update the FTNTFGTeventtime
+    ftntfgt_event_time_match = re.search(r'FTNTFGTeventtime=(\d+)', syslog_line)
+    if ftntfgt_event_time_match:
+        current_time_ns_str = str(current_time_ns)
+        syslog_line = re.sub(r'FTNTFGTeventtime=\d+', f'FTNTFGTeventtime={current_time_ns_str}', syslog_line)
+    
+    return syslog_line
 
-        # Get the current time in the format matching the syslog timestamp
-        if i == 0:
-            # Only modify the timestamp of the first line
-            current_time = datetime.datetime.utcnow().strftime('%b %d %H:%M:%S')
-        else:
-            # Use the timestamp of the first line as a reference
-            current_time = (datetime.datetime.utcnow() +
-                            datetime.timedelta(seconds=i)).strftime('%b %d %H:%M:%S')
+# Function to fetch syslog data from a URL
+def fetch_syslog_data(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.text.splitlines()
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching syslog data: {e}")
+        return []
 
-        # Replace the timestamp in the syslog line with the current time
-        line = line.replace(timestamp_str, current_time, 1)
+# URL of the syslog data file on GitHub
+syslog_data_url = 'https://raw.githubusercontent.com/nosfera0x2/fileZone/main/Fortinet-Grouped-Sept1.txt'
 
-        # Extract the epoch timestamp from the syslog line
-        epoch_str = line.split('FTNTFGTeventtime=')[1].split(' ')[0]
-
-        # Get the current time as a datetime object
-        current_datetime = datetime.datetime.utcnow()
-
-        # Calculate the epoch timestamp in seconds (as an integer)
-        epoch_seconds = int((current_datetime - datetime.datetime(1970, 1, 1)).total_seconds())
-
-        # Convert epoch_seconds to nanoseconds
-        current_time_ns = epoch_seconds * 1_000_000_000
-
-        # Replace the epoch timestamp in the syslog line with the current time
-        line = line.replace(epoch_str, str(current_time_ns))
-
-    # Print or save the updated line as needed
-    print(line)
+syslog_data = fetch_syslog_data(syslog_data_url)
+    
+for syslog_line in syslog_data:
+    updated_line = update_syslog_line(syslog_line)
+        
+    # Send the syslog to the syslog server
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+        s.sendto(updated_line.encode(), syslog_server_address)
+        
+    print(f"Sent: {updated_line}")
+    
+    #time.sleep(60)
